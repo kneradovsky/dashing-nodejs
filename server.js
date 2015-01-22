86595151faa04e8f96435f1beaf9314a949b9492
Konstantin <kneradovsky@gmail.com>
2015-01-22 15:12:19 +0000

    server js added post handler

--- server.js
+++ server.js
@@ -12,22 +12,24 @@ var wsserver = new WebSocketEvents({port: 8080,path:'/websocket/connection'});
 var router = express();
 var server = http.createServer(router);
 
+function consumePostData(res) {
+    var data = new Buffer();
+    while(chunk=res.read()) data.write(chunk.toString());
+    return data.toString();
+}
+
 router.post('/data/:id',function(req,res) {
    id=req.params.id;
-   body=''
-   res.on('data',function(chunk) {
-       body+=chunk.toString();
-   });
-   res.on('end',function() {
-       bodyjson=null;
-       try {
-           bodyjson=JSON.parse(body);
-       } catch(err) {
-           res.statusCode = 400;
-           return res.end('error: ' + er.message);
-       }
+   body=consumePostData(res);
+    bodyjson=null;
+    try {
+       bodyjson=JSON.parse(body);
+    } catch(err) {
+       res.statusCode = 400;
+       return res.end('error: ' + er.message);
+    }
        async(wsserver.send_event(id,bodyjson));
        res.statusCode=204;
        res.end();
-   })
 });
+
